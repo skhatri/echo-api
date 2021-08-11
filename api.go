2329e9221e7d6f9f7f0331dc056cf555eb877106
Suresh Khatri <servlet2@msn.com>
2021-08-12 09:16:45 +1000

    print raw output

--- api.go
+++ api.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/skhatri/api-router-go/router"
 	"github.com/skhatri/api-router-go/router/functions"
+	"github.com/skhatri/api-router-go/router/model"
 	"log"
 	"net/http"
 )
+
+func Echo(request *router.WebRequest) *model.Container {
+	container := model.Response(request)
+	for k, v := range request.Headers {
+		container.AddHeader(k, v)
+	}
+	printData(container)
+	return container
+}
+
+func printData(container *model.Container) {
+	buff := bytes.Buffer{}
+	err := json.NewEncoder(&buff).Encode(container)
+	if err == nil {
+		fmt.Println(buff.String())
+	}
+}
 
 func main() {
 
@@ -15,8 +36,8 @@ func main() {
 		}).
 		Configure(func(configurer router.ApiConfigurer) {
 			configurer.Get("/", functions.IndexFunc)
-			configurer.Get("/echo", functions.EchoFunc)
-			configurer.Post("/echo", functions.EchoFunc)
+			configurer.Get("/echo", Echo)
+			configurer.Post("/echo", Echo)
 		}).
 		Build()
 
